115e83408a1e4e925242d92cec561b56e8182503
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-09-19 17:25:49 +0530

    updated

--- task1/script.js
+++ task1/script.js
@@ -2,3 +2,23 @@ let shoppingCart=document.querySelector('.shopping-cart');
 document.querySelector('#card-btn').onclick=()=>{
     shoppingCart.classList.toggle('active');
 }
+
+let profilelog=document.querySelector('.prolog');
+document.querySelector('#log-btn').onclick=()=>{
+    profilelog.classList.toggle('active');
+}
+
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="admin" && password=="user")
+    {
+        alert("login success");
+        return false
+    }
+    else{
+        alert("login failed");
+    }   
+
+}
